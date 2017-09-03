03430488721f862708c225647ac891529bfb85ad
Christophe Raffalli <raffalli@univ-savoie.fr>
2017-09-03 03:02:27 +0200

    Removed useless functions

--- earley.ml
+++ earley.ml
@@ -381,16 +381,6 @@ let first_pos pos1 pos2 =
   | None -> pos2
   | Some _ -> pos1
 
-let get_pos pos1 pos2 =
-  match pos1 with
-  | None -> pos2
-  | Some (pos1, _) -> pos1
-
-let get_pos_ab pos1 pos2 =
-  match pos1 with
-  | None -> pos2
-  | Some (_, pos1) -> pos1
-
 let apply_pos_debut: type a b.a pos
                      -> (position * position) option
                      -> position -> position -> a =
@@ -880,9 +870,9 @@ let rec one_prediction_production
           }
         in
         let f = fix_begin f pos_ab in
-        begin match pre_rule rest2 with
-        | Empty (g) when debut <> None ->
-          let g = fix_begin g (get_pos_ab debut pos_ab) in
+        begin match pre_rule rest2, debut with
+        | Empty (g), Some(_,pos') ->
+          let g = fix_begin g pos' in
           if !debug_lvl > 1 then Printf.eprintf "RIGHT RECURSION OPbiTIM %a\n%!" print_final element0;
           iter_rules (fun r ->
             let complete = protect errpos (function
