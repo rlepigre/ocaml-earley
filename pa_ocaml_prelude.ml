8964672251c0ab57cc30473d2906f91a547a9e8a
Christophe Raffalli <raffalli@univ-savoie.fr>
2017-09-12 01:35:49 +0200

    forgot to move back print_location after some debugging

--- pa_ocaml_prelude.ml
+++ pa_ocaml_prelude.ml
@@ -60,6 +60,12 @@ let fast  : bool ref          = ref false
 let file  : string option ref = ref None
 let ascii : bool ref          = ref false
 
+let print_location ch {Location.loc_start = s ; Location.loc_end = e} =
+  let open Lexing in
+  Printf.fprintf ch "Position %d:%d to %d:%d%!"
+    s.pos_lnum (s.pos_cnum - s.pos_bol)
+    e.pos_lnum (e.pos_cnum - e.pos_bol)
+
 let string_location {Location.loc_start = s ; Location.loc_end = e} =
   let open Lexing in
   Printf.sprintf "Position %d:%d to %d:%d%!"
