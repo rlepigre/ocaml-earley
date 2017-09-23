3e306201183e5248a0deebf21eefcd298a0f046b
craff <raffalli@univ-savoie.fr>
2017-09-23 05:30:47 +0200

    tests/blank.ml

--- tests/blank.ml
+++ tests/blank.ml
@@ -1,4 +1,4 @@
-open Decap
+open Earley
 
 let parser g = {' ' | '\n' | '\t' | '\r'}* -> ()
 
@@ -6,10 +6,6 @@ let blank = blank_grammar g no_blank
 
 let parser r = {'a' | 'b'}*
 
-let _ = debug_lvl := 2
-(*
-let test = parse_string r blank " a b a b b ab   ab bba "
-*)
 let parser patocomment =
   (change_layout (
     parser
@@ -35,4 +31,8 @@ let blank_mline = blank_grammar blank_grammar_mline no_blank
 let blank1 = blank_grammar patocomments blank_sline
 let blank2 = blank_grammar patocomments blank_mline
 
-let test = parse_string r blank1 "a  aab aa  a a a a\na b a ba b "
+open Common
+
+let _ = parse_string r blank " a b a b b ab   ab bba "
+
+let _ = parse_string r blank1 "a  aab aa  a a a a\na b a ba b "
