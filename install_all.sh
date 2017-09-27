aef6b0a4c49ed1f2524a48278506e28937821669
craff <raffalli@univ-savoie.fr>
2017-09-27 16:47:52 +0200

    added a scrit to install on many opam switch

---
+++ install_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export MAKE="make"
+
+set -v
+
+GOOD=""
+VERSIONS="4.05.0 4.04.2 4.04.1 4.04.0 4.03.0"
+
+function build {
+    opam switch $1
+    eval `opam config env`
+    $MAKE distclean && $MAKE && $MAKE
+    GOOD="$1 , $GOOD"
+}
+
+for v in $VERSIONS; do
+    build $v
+done
+
+$MAKE distclean
