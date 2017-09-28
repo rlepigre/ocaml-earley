d2ffd10aa7f412ba014f5aaf96ae016b8ea0021b
craff <raffalli@univ-savoie.fr>
2017-09-28 22:34:38 +0200

    install_all.sh forgot 4.02.3

--- install_all.sh
+++ install_all.sh
@@ -6,7 +6,7 @@ set -v
 
 SAVE=`opam config var switch`
 GOOD=""
-VERSIONS="4.05.0 4.04.2 4.04.1 4.04.0 4.03.0 4.02.2 4.02.1 4.02.0"
+VERSIONS="4.05.0 4.04.2 4.04.1 4.04.0 4.03.0 4.02.3 4.02.2 4.02.1 4.02.0"
 
 function build {
     opam switch $1
