e790ddfc41ecd1eaf4fbd8cd40c7e5493d8c02ff
craff <raffalli@univ-savoie.fr>
2017-10-02 15:03:50 +0200

    install_all.sh for 4.06 and 4.07

--- install_all.sh
+++ install_all.sh
@@ -6,11 +6,12 @@ set -v
 
 SAVE=`opam config var switch`
 GOOD=""
-VERSIONS="4.05.0 4.04.2 4.04.1 4.04.0 4.03.0 4.02.3 4.02.2 4.02.1 4.02.0"
+VERSIONS="4.07.0+trunk 4.06.0+trunk 4.05.0 4.04.2 4.04.1 4.04.0 4.03.0 4.02.3 4.02.2 4.02.1 4.02.0"
 
 function build {
     opam switch $1
     eval `opam config env`
+    opam install ocamlbuild ocamlfind
     $MAKE distclean && $MAKE && $MAKE install
     GOOD="$1 , $GOOD"
 }
