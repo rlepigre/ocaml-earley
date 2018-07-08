1170a7352423076d1f4611c941e6ad5f2df0b520
craff <raffalli@univ-savoie.fr>
2018-07-08 05:09:57 +0200

    added a file to compile many versions (usefull when bootstraping earley-ocaml

---
+++ allversions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# compile many versions with opam. Usefull one changes some combinators and
+# want to bootstrap earley-ocaml after that.
+
+VERSIONS="4.07.0+trunk 4.06.1 4.06.0 4.05.0 4.04.2 4.04.1 4.04.0 4.03.0"
+
+for v in $VERSIONS; do
+    opam switch $v
+    eval `opam config env`
+    make distclean && make && make install
+done
