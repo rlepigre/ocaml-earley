38bf549b67067240636290739a56fe0905ce8520
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-09-19 07:52:41 +0200

    Script update.

--- allversions.sh
+++ allversions.sh
@@ -3,7 +3,7 @@
 # compile many versions with opam. Usefull one changes some combinators and
 # want to bootstrap earley-ocaml after that.
 
-VERSIONS="4.07.0+trunk 4.06.1 4.06.0 4.05.0 4.04.2 4.04.1 4.04.0 4.03.0"
+VERSIONS="4.07.0 4.06.1 4.06.0 4.05.0 4.04.2 4.04.1 4.04.0 4.03.0"
 
 for v in $VERSIONS; do
     opam switch $v
